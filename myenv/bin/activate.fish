574aecf7d0dc27e9db5f6af507fc11ca5ec8d999
shibil-rahman <shibilr6@gmail.com>
2023-08-14 09:03:29 +0530

    Add output CSV file from Jenkins

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shibilrahmanp/.jenkins/workspace/ai-sast-agent-2/myenv"
+set -gx VIRTUAL_ENV "/Users/shibilrahmanp/.jenkins/workspace/ai-sast-agent/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
